0db831c81878716200dbf2794d8a30dfb619d493
Joseph LeGrand <jrlegrand@gmail.com>
2023-12-28 20:26:57 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,7 +69,7 @@ const config = {
         title: 'SageRx',
         logo: {
           alt: 'SageRx Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
